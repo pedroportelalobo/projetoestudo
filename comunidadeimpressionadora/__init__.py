33789eb89a148c5f28c5b38c958055a982b98a5b
Pedro Portela <pedro.portela.lobo@gmail.com>
2025-03-21 20:09:21 -0300

    Atualizações no Init

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -4,6 +4,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
 from dotenv import load_dotenv
+import sqlalchemy
 
 load_dotenv()
 
@@ -18,5 +19,17 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message = 'Faça login para continuar'
 login_manager.login_message_category = 'alert-info'
+
+from comunidadeimpressionadora import models
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sqlalchemy.inspect(engine)
+if not inspector.has_table("usuario"):
+    with with app.app_contex():
+        database.drop_all()
+        database.create_all()
+        print("Base de dados Criada")
+else:
+    print("Base de dados inexistente")
+
 
 from comunidadeimpressionadora import routes
