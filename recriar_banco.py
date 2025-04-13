447a49337186ebc73c81aaf70afccc317f7b1682
pedroportelalobo <pedro.portela.lobo@gmail.com>
2025-04-13 10:26:11 -0300

    Update recriar_banco.py

--- recriar_banco.py
+++ recriar_banco.py
@@ -2,4 +2,5 @@ from comunidadeimpressionadora import app, database
 from comunidadeimpressionadora.models import Usuario, Post, Comentario
 
 with app.app_context():
+    database.drop_all()
     database.create_all()
